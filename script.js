3df3c167494e5a066ea68a904bc0011884068a42
Alex Nash <Alexr.nash@gmail.com>
2023-01-07 01:01:35 -0500

    added arrays to JS

--- script.js
+++ script.js
@@ -24,3 +24,9 @@ function printTrue() {
 }
 
 start.addEventListener('click', startTime)
+
+// will move arrays into functions after timer fix 
+const questions = ["Commonly used data types DO NOT include:", "Arrays in JavaScript can be use to store:", "What symbol must JavaScript functions be wrapped in?"]
+const ans1 = ["1. Strings", "2. Integers", "3. alerts", "4. Booleans"]
+const ans2 = ["1. Numbers and strings", "Other arrays", "Booleans", "All of the above"]
+const ans3 = ["1. ()", "2. !", "3. {}", "4.''"]
